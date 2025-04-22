e541f2e7639694543cccbfd263cfc036b6ba88a0
KaylaCrush <kayla.e.rieck@gmail.com>
2025-04-22 13:47:01 -0700

    Initial cleanup

--- src/boid/components.rs
+++ src/boid/components.rs
@@ -19,20 +19,3 @@ pub struct BoidBundle<M: Material2d> {
     pub material: MeshMaterial2d<M>,
     pub transform: Transform,
 }
-
-// impl<M: Material2d> BoidBundle<M> {
-//     pub fn new(
-//         mesh: Handle<Mesh>,
-//         material: Handle<M>,
-//         transform: Transform,
-//     ) -> Self {
-//         Self {
-//             boid: Boid,
-//             velocity: Velocity(Vec3::ZERO),
-//             acceleration: Acceleration(Vec3::ZERO),
-//             mesh: Mesh2d(mesh),
-//             material: MeshMaterial2d(material),
-//             transform,
-//         }
-//     }
-// }
